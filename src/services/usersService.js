4f10cd29a2305276de0ee229776c2236aba5c424
Paulo <paulodutra247@gmail.com>
2026-05-18 21:44:35 -0300

    fix little name issue

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,7 +1,7 @@
 import formatTimeStamp from "../utils/dateFormat.js"
 import criptPassword from "../utils/cripto.js"
 import AppError from "../utils/errorCatch.js"
-import {create, findAll, findById, deleteById, patchUpdate, updateAllProperty, findEmail} from "../repository/userRepository.js"
+import {create, findAll, findById, deleteById, patchUpdate, updateAllProperty, findEmail} from "../repository/usersRepository.js"
 import { isValidEmail, normalizeEmail } from "../utils/emailUtils.js"
 import { captalizeWords } from "../utils/wordsUtils.js"
 
